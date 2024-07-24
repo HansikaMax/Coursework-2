ed8e2f1f1d458de5162e6397eae1cc1f9303a7b4
HansikaMax <praveenweeratunga04@gmail.com>
2024-07-24 20:32:06 +0530

    Created Login Controller and Home.fxml

---
+++ src/controller/LoginController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+
+public class LoginController {
+    
+     @FXML
+    private AnchorPane root;
+
+    @FXML
+    private Rectangle rectangle;
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    private JFXButton btnEnter;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void enterOnAction(ActionEvent event) throws IOException {
+        String username = txtUserName.getText();
+        String password = txtPassword.getText();
+
+        if (isValidCredentials(username, password)) {
+           new Alert(Alert.AlertType.CONFIRMATION,"Login successful!").show();
+
+           this.root.getChildren().clear();
+           Parent node = FXMLLoader.load(this.getClass().getResource("/view/Home.fxml"));
+           this.root.getChildren().add(node);
+        } else {
+            new Alert(Alert.AlertType.ERROR,"Wrong username or password").show();
+        }
+    }
+    private boolean isValidCredentials(String username, String password) {
+        // Hardcoded credentials for demonstration purposes
+        String validUsername = "admin";
+        String validPassword = "password";
+
+        return username.equals(validUsername) && password.equals(validPassword);
+    }
+}
