c8d76e0f68db6e9c1a5b8f75916d2a955cd8a84a
HansikaMax <praveenweeratunga04@gmail.com>
2024-07-26 15:16:32 +0530

    created HomeController

---
+++ src/controller/HomeController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HomeController {
+    
+      @FXML
+    private JFXButton btnBookType;
+
+    @FXML
+    private JFXButton btnBooks;
+
+    @FXML
+    private JFXButton btnBorrowBooks;
+
+    @FXML
+    private JFXButton btnMembers;
+
+    @FXML
+    private JFXButton btnOverdueMembers;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    void bookTypesOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void booksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrowBooksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void membersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void overdueMembersOnAction(ActionEvent event) {
+
+    }
+
+}
