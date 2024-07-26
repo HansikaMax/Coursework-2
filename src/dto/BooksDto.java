a6c99aae023c70cc0f19160bd57d02241fb99705
HansikaMax <praveenweeratunga04@gmail.com>
2024-07-26 21:43:47 +0530

    Created BookDto,BookEntity

---
+++ src/dto/BooksDto.java
@@ -0,0 +1,64 @@
+package dto;
+
+public class BooksDto {
+    private int bookID;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String categoryName;
+   
+    public BooksDto() {
+    }
+
+    public BooksDto(int bookID, String title, String author, int publicationYear, String categoryName) {
+        this.bookID = bookID;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.categoryName = categoryName;
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    
+   
+
+}
