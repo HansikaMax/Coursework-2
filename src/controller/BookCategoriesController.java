5bdf62921abd61ec0fa910be2e33ac6da8489ab9
HansikaMax <praveenweeratunga04@gmail.com>
2024-07-31 13:57:03 +0530

    Created save, update, delete methods for BookCategories and also finished creating  the tableview to view Book Categories

--- src/controller/BookCategoriesController.java
+++ src/controller/BookCategoriesController.java
@@ -1,14 +1,23 @@
 package controller;
 
+import java.util.ArrayList;
+
 import com.jfoenix.controls.JFXButton;
 
 import dto.BookCategoriesDto;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import service.ServiceFactory;
+import service.custom.BookCategoriesService;
 
 public class BookCategoriesController {
     
@@ -55,17 +64,112 @@ public class BookCategoriesController {
     private TextField txtDescription;
 
     @FXML
-    void deleteOnAction(ActionEvent event) {
+    void deleteOnAction(ActionEvent event) throws Exception {
+
+       try {
+       Integer categoryID=Integer.parseInt(txtCategoryID.getText());
+       delete(categoryID);
+       new Alert(Alert.AlertType.CONFIRMATION,"Book Category Deleted Successfully!!!").show();
+       clearForm();
+       loadTable();
+
+       } catch (Exception e) {
+        e.printStackTrace();
+        new Alert(Alert.AlertType.ERROR,"Error While Deleting Book Category").show();
+       }
 
     }
 
     @FXML
     void saveOnAction(ActionEvent event) {
+     try {
+        BookCategoriesDto bcDto=new BookCategoriesDto(Integer.parseInt(txtCategoryID.getText()),txtCategoryName.getText(),txtDescription.getText());
+        save(bcDto);
+         new Alert(Alert.AlertType.CONFIRMATION,"Book Category Saved Successfully!!!").show();
+         clearForm();
+         loadTable();
+     } catch (Exception e) {
+        e.printStackTrace();
+        new Alert(Alert.AlertType.ERROR,"Error While Saving Book Category").show();
+     }
+
 
     }
 
     @FXML
     void updateOnAction(ActionEvent event) {
 
+       try {
+            BookCategoriesDto bcDto=new BookCategoriesDto(Integer.parseInt(txtCategoryID.getText()),txtCategoryName.getText(),txtDescription.getText());
+            update(bcDto);
+             new Alert(Alert.AlertType.CONFIRMATION,"Book Category Updated Successfully!!!").show();
+             clearForm();
+             loadTable();
+         } catch (Exception e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,"Error While Updating Book Category").show();
+         }
+    
+    }
+
+
+    @FXML
+    void selectOnMouseClicked(MouseEvent event) {
+   BookCategoriesDto selectBCDto=tblBookCategories.getSelectionModel().getSelectedItem();
+   txtCategoryID.setText(Integer.toString(selectBCDto.getCategoryID()));
+   txtCategoryName.setText(selectBCDto.getCategoryName());
+   txtDescription.setText(selectBCDto.getDescription());
+    }
+
+
+///////////
+    private BookCategoriesService bcService = (BookCategoriesService)ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.BookCategories);
+    
+    public String save(BookCategoriesDto bcDto) throws Exception{
+       return bcService.save(bcDto);
+    }
+
+    public String update(BookCategoriesDto bcDto) throws Exception{
+       return bcService.update(bcDto);
+    }
+
+    public String delete(Integer categoryID) throws Exception{
+       return bcService.delete(categoryID);
+    }
+
+    public ArrayList<BookCategoriesDto> getAll() throws Exception{
+       return bcService.getAll();
+    }
+
+    public BookCategoriesDto get(String categoryID) throws Exception{
+        return null;
+    }
+
+
+////////////
+
+ private void loadTable() throws Exception {
+       
+       ArrayList<BookCategoriesDto> bookCategoriesList=getAll();
+      ObservableList<BookCategoriesDto> bcObservableList = FXCollections.observableArrayList();
+      
+      for(BookCategoriesDto bcDto: bookCategoriesList){
+        bcObservableList.add(bcDto);
+      }
+      tblBookCategories.setItems(bcObservableList);
+    }
+     
+    public void initialize() throws Exception {
+           colCategoryID.setCellValueFactory(new PropertyValueFactory<>("categoryID"));
+        colCategoryName.setCellValueFactory(new PropertyValueFactory<>("categoryName"));
+      colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        
+        loadTable();
+    }
+
+    private void clearForm(){
+        txtCategoryID.setText("");
+        txtCategoryName.setText("");
+        txtDescription.setText("");
     }
 }
